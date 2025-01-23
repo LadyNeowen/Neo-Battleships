5b35cda428d6ce238ffb160eea9f3e62071881e4
LadyNeowen <Alicia.Westerlund@outlook.com>
2025-01-23 15:18:52 +0000

    Adding randint for random number generator, a board class and a function for creating the board

--- run.py
+++ run.py
@@ -1,3 +1,24 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random import randint
+scores = {'computer': 0, 'player': 0}
+
+class Board:
+    """
+    The main board class which sets which board size the player choses,
+    the number of ships, the players name input and the board type.
+    (Player board or computers board)
+    Has methods for adding ships depending on board size,
+    the guesses made by player and computer and printing the board.
+    """
+        def __init__(self, size, num_ships, name, type):
+            self.size = size
+            self.board = [['.' for x in range(size)] for y in range(size)]
+            self.num_ships = num_ships
+            self.name = name
+            self.type = type
+            self.guesses = []
+            self.ships = []
+
+        def print(self):
+            for row in self.board:
+                print(' '.join(row))
+
