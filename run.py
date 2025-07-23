eea90bf0bd1ba20cbfc05b1a21431919981313a4
LadyNeowen <Alicia.Westerlund@outlook.com>
2025-07-23 20:13:17 +0000

    Mapping out placeholders for my functions and what i want them to do and giving them a pass criteria for now.

--- run.py
+++ run.py
@@ -1,7 +1,39 @@
 
 
 # Creates the board
-
 def create_board():
-    return [[' ' for x in range(8)] for x in range(8)]
-    
+    return [['~' for x in range(8)] for x in range(8)]
+
+# Show the board to the player
+def print_board():
+    pass
+
+# Randomly place 5 ships on the board
+def place_ships():
+    pass
+
+# Ask the player where to shoot
+def get_player_move():
+    pass
+
+# Make the computer pick a random spot
+def get_computer_move():
+    pass
+
+# Check if a ship is hit
+def ships_hit():
+    pass
+
+# Counts how many ships are hit
+def counts_ships():
+    pass
+
+# Main game function
+def play_game():
+    pass
+
+
+
+# Start the game
+#play_game()
+
