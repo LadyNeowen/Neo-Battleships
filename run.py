b8e3accf73e75212efbc09466ae2a4d080bba4fe
LadyNeowen <Alicia.Westerlund@outlook.com>
2025-08-05 21:19:17 +0000

    Restarting the code, new initial commit with legend and a function to print my intro at the begginig of each new game

--- run.py
+++ run.py
@@ -1,62 +1,10 @@
-from random import randint
+"""
+Python Battleship Game
+Legend
 
-# Creates the board
-def create_board():
-    return [['~' for x in range(8)] for x in range(8)]
-    
-
-# Show the board to the player
-def print_board(board):
-    print('  A B C D E F G H')
-    print('--------------')
-    row_number = 1
-    for row in board:
-        print(str(row_number) + '|' + ' '.join(row) + '|')
-
-
-# Randomly place 5 ships on the board
-def place_ships(board):
-    for ship in range(5):
-        ship_row, ship_column = randint(0,7), randint(0,7)
-        while board[ship_row][ship_column] == "X":
-            ship_row, ship_column = randint(0,7), randint(0,7)
-        board[ship_row][ship_column] = "X"
-
-# Ask the player where to shoot
-def get_player_move(rows, columns):
-    """
-    Asks the player to make a move, for example "A5").
-    Input is then converted into a row and column on the board.
-    """
-    while True:
-        try:
-            guess = input("Enter your guess (For example, 'A5'):  \n").strip().upper()
-            col, row = ord(guess[0]) - ord('A'), int(guess[1:]) - 1
-            if 0 <= row < rows and 0 <= col < columns:
-                return row, col
-            else:
-                print("Invalid guess. Try again. \n")
-        except (ValueError, IndexError, TypeError):
-            print("Invalid guess. Try again. \n")
-
-# Make the computer pick a random spot
-def get_computer_move():
-    pass
-
-# Check if a ship is hit
-def ships_hit():
-    pass
-
-# Counts how many ships are hit
-def counts_ships():
-    pass
-
-# Main game function
-def play_game():
-    pass
-
-
-
-# Start the game
-#play_game()
+'X' for hit battleship
+'~' for available space
+'O' for missed shot
+'@' for placed battleship
+"""
 
