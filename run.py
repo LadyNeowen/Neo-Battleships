0bf263c228cf4e0c3dc0a5d08a40e202e1814f8f
LadyNeowen <Alicia.Westerlund@outlook.com>
2025-08-05 22:11:02 +0000

    Fixed synthax error with comments

--- run.py
+++ run.py
@@ -1,24 +1,25 @@
-"""
-Python Battleship Game
-Legend
+import random
 
-'X' for hit battleship
-'~' for available space
-'O' for missed shot
-'@' for placed battleship
-"""
 
+"""
+Function to play the game intro
+"""
 def print_intro():
     print(" ..You wake up with a throbbing ache \n at the back of your skull.\n As you rise up slowly your eyes widen..\n You are standing on the deck of a \n giant ship! \n All of a sudden a cannonblast goes off \n right next to you and a man shouts: \n Hey you!! What's your name? \n Nevermind that! YOU are in charge now! \n Where should we aim the cannons \n captain?!")
     print()
     print()
 
-
+"""
+Function to print the board with numbers as columns and letters as rows 1-5 A-E
+"""
 def print_board(board):
     print(" ", " ".join("12345"))
     for letter, row in zip("ABCDE", board):
         print(letter, " ".join(row))
 
+"""
+Function to place 4 random ships on the board
+"""
 def place_ships(board, num_ships=4):
     placed = 0
     while placed < num_ships:
@@ -28,6 +29,10 @@ def place_ships(board, num_ships=4):
             board[row][col] = "@"
             placed += 1
 
+"""
+Asking the player for an input but instead of asking for column and row separately instead making it a single input,
+as well as making sure the loop repeats util it gets a an approved input.
+"""
 def shoot():
     while True:
         guess = input("Aim the cannons!!! (e.g. A5) \nSink these blasted land lubbers \nright into the abyss! \n ").upper()
@@ -51,6 +56,10 @@ def shoot():
         col_index = col_number - 1
         return row_index, col_index
 
+
+"""
+Checking if the guessed input was a hit or a miss and printing the appropriet messages.
+"""
 def check_hit(hidden_board, player_board, row, col):
     if hidden_board[row][col] == "@":
         print("It's a HIT!!! 💣💥 Well done captain! ")
@@ -61,8 +70,11 @@ def check_hit(hidden_board, player_board, row, col):
         player_board[row][col] = "O"
         return False
 
+
+"""
+Making the play game function with player name input, a welcoming message with the rules as well as creating the board of 5*5
+"""
 def play_game():
-    # Intro and setup
     print_intro()
     player_name = input("Please enter your name: ")
     print(f"\n Welcome to NeoBattleships, {player_name}!")
@@ -72,7 +84,11 @@ def play_game():
     hidden_board = [["~"] * 5 for _ in range(5)]
     place_ships(hidden_board)
 
-    # Game loop
+
+    """
+    Making the game loop with maximum hits for the same amount of ships there are, a maximum of turns and a turn count,
+    also while checking for a correct input before breaking out of the loop and increasing the count    
+    """
     hits = 0
     max_hits = 4
     turn = 1
@@ -100,14 +116,20 @@ def play_game():
 
         turn += 1
 
-        # End of game
+    """
+    Checking for the end game to see if all the ships were hit or if the amount of turns were up and printing the message accordingly
+    """
     print_board(player_board)
     if hits == max_hits:
         print("\nCongratulations! You sank all the ships! 💥🏴‍☠️")
     else:
         print("\nGame Over!!💀💀💀 \nYou ran out of cannonballs...💣")
 
-         # Ask to play again
+
+    """
+    Checking after the game is over if the player wants to play again, if so restarting the intro and name input,
+    if not then printing a goodbye message and breaking out of the loop
+    """
     while True:
         again = input("\nWould you like to play again, captain? \n(Y/N): ").strip().upper()
         if again == "Y":
@@ -120,5 +142,5 @@ def play_game():
         else:
             print("Just Y or N, cap'n!")
 
-# Start the first game
+
 play_game()
